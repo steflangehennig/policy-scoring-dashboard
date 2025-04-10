bfc5546c523591dc2d11c7c184418974c612308a
Stefani Langehennig <stefani.langehennig@colorado.edu>
2025-04-10 13:48:17 -0600

    update react app

---
+++ src/ProfileCard.js
@@ -0,0 +1,13 @@
+// src/ProfileCard.js
+
+function ProfileCard(props) {
+    return (
+      <div>
+        <h2>{props.name}</h2>
+        <p>{props.title}</p>
+      </div>
+    );
+  }
+  
+  export default ProfileCard;
+  
