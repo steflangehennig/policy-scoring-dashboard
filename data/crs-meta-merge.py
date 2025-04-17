0599f962d762910b4748dfa6c6efa7103922c06c
Stefani Langehennig <stefani.langehennig@colorado.edu>
2025-04-17 16:45:38 -0600

    file reorg

--- data/crs-meta-merge.py
+++ data/crs-meta-merge.py
@@ -7,8 +7,8 @@ scores["number"] = scores["filename"].str.replace(".txt", "", regex=False)
 # Pull in CRS metadata for merge
 reports_meta = pd.read_csv("reports.csv")
 
-# Merge by CRS report number
+# Merge by CRS report number - doublecheck it matches meta data
 merged_df = scores.merge(reports_meta, on="number", how="left")
 
 # Save final enriched data
-merged_df.to_csv("scored_crs_reports.csv", index=False)
+merged_df.to_csv("scored_crs_reports.csv", index=False) 
